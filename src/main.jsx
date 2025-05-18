08e0cbdd7507e65fa8af06a2b4e200874965215a
Ashutosh <ashutoshorisoh@gmail.com>
2025-05-18 11:28:02 +0530

    vercel analytics enabled

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { RecoilRoot } from 'recoil';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <RecoilRoot>
             <App />
+            <Analytics />
         </RecoilRoot>
     </React.StrictMode>
 ); 
